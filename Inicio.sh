077c5fcbb93ca027d8d0aa4bab6fa24b3be316f3
Lucas N Nobrega <lucasnnobrega@gmail.com>
2020-11-27 09:58:34 -0300

    del Inicio.sh

--- Inicio.sh
+++
@@ -1,7 +0,0 @@
-
-rosservice call /uav1/control_manager/goto "goal: [-123120.783862, 1108535.74603, 12.72548772012, 0.0]"
-sleep 20
-rosservice call /uav1/control_manager/goto "goal: [-123120.783862, 1108535.74603, 1.72548772012, 0.0]"
-sleep 20
-rosservice call /uav1/control_manager/goto_relative "goal: [0, 0, 0, 4.83]"
-   
